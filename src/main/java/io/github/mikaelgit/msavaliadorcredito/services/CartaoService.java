fa56a9cf59422f216658f0710a77d19b3cff910e
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-08-01 23:58:42 -0300

    Aadicionando chamada sincrona com openfeign ao microserviço de cartões para analisar situação do cliente

--- src/main/java/io/github/mikaelgit/msavaliadorcredito/services/CartaoService.java
+++ src/main/java/io/github/mikaelgit/msavaliadorcredito/services/CartaoService.java
@@ -1,9 +1,18 @@
 package io.github.mikaelgit.msavaliadorcredito.services;
 
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import io.github.mikaelgit.msavaliadorcredito.dtos.DadosCartaoDTO;
 
+@FeignClient(value = "mscartoes", path = "/cartoes")
 public interface CartaoService {
 
-    DadosCartaoDTO consultarDadosCartaoPorCliente(String cpf);
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<DadosCartaoDTO>> getCartoesByCliente(@RequestParam("cpf") String cpf);
     
 }
--- src/main/java/io/github/mikaelgit/msavaliadorcredito/services/impl/AvaliadorServiceImpl.java
+++ src/main/java/io/github/mikaelgit/msavaliadorcredito/services/impl/AvaliadorServiceImpl.java
@@ -1,25 +1,35 @@
 package io.github.mikaelgit.msavaliadorcredito.services.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.github.mikaelgit.msavaliadorcredito.dtos.DadosCartaoDTO;
 import io.github.mikaelgit.msavaliadorcredito.dtos.DadosClienteDTO;
 import io.github.mikaelgit.msavaliadorcredito.dtos.SituacaoClienteDTO;
 import io.github.mikaelgit.msavaliadorcredito.services.AvaliadorCreditoService;
+import io.github.mikaelgit.msavaliadorcredito.services.CartaoService;
 import io.github.mikaelgit.msavaliadorcredito.services.ClienteService;
 
-@Service 
+@Service
 public class AvaliadorServiceImpl implements AvaliadorCreditoService {
 
     @Autowired
     private ClienteService clienteService;
 
+    @Autowired
+    private CartaoService cartaoService;
+
     @Override
     public SituacaoClienteDTO obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosClienteDTO> dadosClienteResponse = clienteService.getClienteByCpf(cpf);
-
-        return SituacaoClienteDTO.builder().dadosCliente(dadosClienteResponse.getBody()).build();
+        ResponseEntity<List<DadosCartaoDTO>> dadosCartaoResponse = cartaoService.getCartoesByCliente(cpf);
+        return SituacaoClienteDTO.builder()
+                .dadosCliente(dadosClienteResponse.getBody())
+                .cartoes(dadosCartaoResponse.getBody())
+                .build();
     }
 
 }
