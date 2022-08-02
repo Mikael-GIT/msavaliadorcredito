c3b21d0c3fa4a0977d9b9f64f3213cd6c98e2244
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-08-01 22:55:59 -0300

    Corrigindo nome metodos

--- src/main/java/io/github/mikaelgit/msavaliadorcredito/services/CartaoService.java
+++ src/main/java/io/github/mikaelgit/msavaliadorcredito/services/CartaoService.java
@@ -1,5 +1,7 @@
 package io.github.mikaelgit.msavaliadorcredito.services;
 
+import io.github.mikaelgit.msavaliadorcredito.dtos.DadosCartaoDTO;
+
 public interface CartaoService {
 
     DadosCartaoDTO consultarDadosCartaoPorCliente(String cpf);
